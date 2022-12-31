b66914e78478b2e858a2b1498464b6a237709d08
catking <edarhinos9@gmail.com>
2023-01-01 02:02:11 +0800

    ver 0.99

--- personal/interact.js
+++ personal/interact.js
@@ -1,3 +1,5 @@
+var prev_scroll_top = 0;
+
 function scroll_effect()
 {
     let view_bottom = $(window).scrollTop() + $(window).height();
@@ -12,4 +14,43 @@ function scroll_effect()
             current.css({top: "0", opacity: "1"});
         }
     }
+}
+
+function nav_show()
+{
+    let nav_bar = $("nav");
+
+    nav_bar.css("top", "0");
+}
+
+function nav_hide()
+{
+    let nav_bar = $("nav");
+
+    nav_bar.css("top", "-8vh");
+}
+
+function nav_effect()
+{
+    let cur_scroll_top = $(window).scrollTop();
+    let on_nav_bar = $("nav:hover").length;     // check if mouse is on nav
+
+    if(cur_scroll_top > prev_scroll_top && !on_nav_bar)    // scorll down -> hide the nav bar
+    {
+        nav_hide();
+    }
+    else
+    {
+        nav_show();
+    }
+
+    prev_scroll_top = cur_scroll_top;
+}
+
+window.onload = function () 
+{
+    scroll_effect();
+
+    $(document).on("scroll", scroll_effect);
+    $(document).on("scroll", nav_effect);
 }
